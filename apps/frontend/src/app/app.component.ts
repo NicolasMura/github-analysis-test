904c7fff8e5dd83c5d0f2940b94cf786f32c825c
NicolasMura <contact@nicolasmura.fr>
2023-09-21 18:55:20 +0200

    Init - Remove demo stuff

--- apps/frontend/src/app/app.component.ts
+++ apps/frontend/src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
-  selector: 'github-analysis-test-root',
+  imports: [RouterModule],
+  selector: 'gat-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
